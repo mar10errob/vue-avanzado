3792e5dd8a5d5983682fb9fce0423ea7b274fae8
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2017-12-28 23:55:36 -0600

    Se dividio en componentes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   #app
+    pm-header
     section.section
       nav.nav.has-shadow
         .container
@@ -18,12 +19,16 @@
         .columns
           .column(v-for="track in tracks")
             | {{ track.name }} - {{ track.artists[0].name }}
+    pm-footer
 </template>
 <script>
 import trackService from './services/track'
+import PmFooter from './components/layout/Footer.vue'
+import PmHeader from './components/layout/Header.vue'
 
 export default {
   name: 'app',
+  components: { PmFooter, PmHeader },
   data () {
     return {
       searchQuery: '',
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+  footer.footer
+    .container
+      .content.has-text-centered
+        p 2017 by @mar10errob
+</template>
